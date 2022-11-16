f920bdc627c60bbf58055a42f891c613be56d8d0
sugu-mar <113677981+sugu-mar@users.noreply.github.com>
2022-11-16 16:43:12 +0530

    Update Task 1 IIFE function.js

--- Task 1 IIFE function.js
+++ Task 1 IIFE function.js
@@ -5,7 +5,7 @@ console.log(`programs in IIFE function`);
 
 
 //(a) Print odd numbers in an array
-console.log("\n ");console.log(`Print odd numbers in an array:`);
+console.log("\n ");console.log(`(a) Print odd numbers in an array:`);
 
 
 
